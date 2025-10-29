fe826f92d6167a8b6fba6f1ba66c575d41c01da4
ttunquist1 <tun21002@byui.edu>
2025-10-28 19:04:01 -0600

    fixed error

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -1,15 +1,17 @@
 """
 Client-side Firebase configuration for Google Sign-In
+Loads configuration from Streamlit secrets
 """
+
+import streamlit as st
 
 # Firebase configuration for client-side (web) authentication
 FIREBASE_CONFIG = {
-    "apiKey": "AIzaSyBrsei2OXneDgVte0SUI6jSSRzEIhMBzvU",
-    "authDomain": "classcrew-45595.firebaseapp.com",
-    "databaseURL": "https://classcrew-45595-default-rtdb.firebaseio.com",
-    "projectId": "classcrew-45595",
-    "storageBucket": "classcrew-45595.firebasestorage.app",
-    "messagingSenderId": "496661610033",
-    "appId": "1:496661610033:web:7b219dfd4e9f1089775f10",
-    "measurementId": "G-THLL64RVBS"
+    "apiKey": st.secrets["firebase"]["web_api_key"],
+    "authDomain": f"{st.secrets['firebase']['project_id']}.firebaseapp.com",
+    "projectId": st.secrets["firebase"]["project_id"],
+    "storageBucket": f"{st.secrets['firebase']['project_id']}.appspot.com",
+    "messagingSenderId": st.secrets["firebase"]["client_id"],
+    "appId": st.secrets["firebase"]["web_app_id"],
+    "measurementId": st.secrets["firebase"]["measurement_id"]
 }
